eb6be13642e945f783412cd9e52e60965fb3b716
yakun.cyk <yakun.cyk@alibaba-inc.com>
2015-10-27 14:57:56 +0800

    .

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,17 @@ var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
 var clean = require('gulp-clean');
+var zip = require('gulp-zip');
+var runSequence = require('run-sequence');
 
+
+gulp.task('clean', function () {
+    return gulp.src('build/*')
+        .pipe(clean());
+});
 
 gulp.task('usemin', function () {
     return gulp.src('src/*.html')
-        .pipe(clean())
         .pipe(usemin({
             css: [minifyCss, rev],
             html: [function () {
@@ -21,6 +27,11 @@ gulp.task('usemin', function () {
 });
 
 gulp.task('manifest', function () {
+    return gulp.src(['src/js/*'])
+        .pipe(gulp.dest('build/src/js'));
+});
+
+gulp.task('manifest', function () {
     return gulp.src(['manifest.json'])
         .pipe(gulp.dest('build/'));
 });
@@ -29,3 +40,23 @@ gulp.task('assets', function () {
     return gulp.src(['assets/*'])
         .pipe(gulp.dest('build/assets/'));
 });
+
+gulp.task('zip', function () {
+    var version = require('./package.json').version;
+    return gulp.src('build/**/*')
+        .pipe(zip('mockit-' + version + '.zip'))
+        .pipe(gulp.dest('release'));
+});
+
+//gulp.task('defaults', function (callback) {
+//    runSequence('clean',
+//        //['assets', 'manifest', 'usemin'],
+//        'zip',
+//        callback);
+//});
+
+gulp.task('default', [
+    //'clean'
+    'usemin',
+     'manifest', 'assets', 'zip'
+]);
